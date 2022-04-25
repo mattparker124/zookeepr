de799bac9e8d6adb7df0979e33afe301f60f0fc8
Matthew Parker <mattparker124@gmail.com>
2022-04-25 17:44:30 -0500

    added get and post functionality for animals display

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
 
@@ -99,6 +101,22 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookepeers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
